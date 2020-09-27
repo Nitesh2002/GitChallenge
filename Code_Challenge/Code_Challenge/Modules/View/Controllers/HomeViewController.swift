6c07731ea34d78a111fb74401ae3559a516e11b5
Nitesh <nitesh@Niteshs-MacBook-Pro-3.local>
2020-09-27 12:32:42 +0530

    Commited Tableview and its cell programtic base layout creation

--- Code_Challenge/Code_Challenge/Modules/View/Controllers/HomeViewController.swift
+++ Code_Challenge/Code_Challenge/Modules/View/Controllers/HomeViewController.swift
@@ -8,23 +8,51 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController{
+    
+    fileprivate let cellResuseIdentifier = "cell"
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .lightGray
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellResuseIdentifier)
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(InfoTableViewCell.self, forCellReuseIdentifier: cellResuseIdentifier)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "TableView Demo"
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        setupAutoLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupAutoLayout() {
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
-    */
+}
 
+
+// MARK: UITableViewDataSource & UITableViewDelegate
+
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellResuseIdentifier, for: indexPath) as! InfoTableViewCell
+        cell.nameLabel.text = "Test Name"
+        return cell
+    }
 }
