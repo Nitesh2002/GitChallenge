cda9ce33b31e73f39b9eb2cfce6f2c649e8eafbc
Nitesh <nitesh@Niteshs-MacBook-Pro-3.local>
2020-09-27 11:56:40 +0530

    Commited code and group structure

---
+++ Code_Challenge/Code_Challenge/Window/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  Code_Challenge
+//
+//  Created by Nitesh on 27/09/20.
+//  Copyright © 2020 Nitesh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        setRootViewController()
+        return true
+    }
+    
+    private func setRootViewController(){
+        let homeVC = HomeViewController()
+        homeVC.view.backgroundColor = .red
+        let navC = UINavigationController(rootViewController: homeVC)
+        self.window?.rootViewController = navC
+    }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+}
+
