24ebc7e5baabafffb2a6fce4cbaf9e37d9c62ee2
Nitesh <nitesh@Niteshs-MacBook-Pro-3.local>
2020-09-27 11:35:02 +0530

    Commited initial project source

---
+++ Code_Challenge/Code_Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code_Challenge
+//
+//  Created by Nitesh on 27/09/20.
+//  Copyright © 2020 Nitesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
